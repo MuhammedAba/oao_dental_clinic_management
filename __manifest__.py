178b511b61c5dcc5b15aa84b7c356ac8ff41b0d7
MuhammedAba <talhaaba456@gmail.com>
2024-10-22 13:00:49 +0300

    added manifest file

---
+++ __manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'Dental Clinic Management',
+    'version': '16.0.0.0.1',
+    'category': 'Healthcare',
+    'summary': 'Dental clinic management system for dentists',
+    'description': """
+        OAO Dental Clinic Management
+        ============================
+        This module helps dentists manage their clinic activities, including patient records, appointments, treatments, and billing.
+    """,
+    'author': 'Muhammed ABA',
+    'website': 'https://omeraba.com/tr/blog/odoo-projeleri-23/dis-klinigi-yonetim-sistemi-48',
+    'depends': ['base', ],
+    'data': [
+    ],
+    'sequence': '-1',
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+}
