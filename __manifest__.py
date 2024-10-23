45f6bc64cd91f5ddccb7a6d3f3228aabf6e61e9f
MuhammedAba <talhaaba456@gmail.com>
2024-10-23 12:26:59 +0300

    added patient model and it's view, added employee type model and it's view , added csv file  for employee types data, added securtiy file for  two models

--- __manifest__.py
+++ __manifest__.py
@@ -8,11 +8,16 @@
         ============================
         This module helps dentists manage their clinic activities, including patient records, appointments, treatments, and billing.
     """,
-    'author': 'Muhammed ABA',
+    'author': 'ABA TECH GROUP',
     'website': 'https://omeraba.com/tr/blog/odoo-projeleri-23/dis-klinigi-yonetim-sistemi-48',
     'depends': ['base', ],
     'data': [
+        'views/patient_views.xml',
+        'views/employee_type_views.xml',
+        'security/ir.model.access.csv',
+        'data/employee.type.csv',
     ],
+    # 'images': ['static/src/description/icon.png'],
     'sequence': '-1',
     'installable': True,
     'application': True,
---
+++ models/patient.py
@@ -0,0 +1,25 @@
+from odoo import api, fields, models
+from datetime import date, timedelta
+
+
+class Patient(models.Model):
+    _name = "patient.patient"
+    _description = "Patients"
+    _order = 'name desc'
+
+    name = fields.Char(string="Patient Name", required=1)
+    surname = fields.Char(string="Patient Surname", required=1)
+    date_of_birth = fields.Date(string='Date Of Birth', default=date.today(), required=1)
+    age = fields.Char(string='Age In Years', compute="compute_age", store=True)
+    image = fields.Image(string="Image")
+    gender = fields.Selection([('male', "Male"), ('female', 'Female')], string='Gender', default='male')
+    note = fields.Text(string="Description")
+    phone = fields.Char(string="Phone", required=1)
+    email = fields.Char(string="Email")
+    blood_type = fields.Selection([
+        ('a-', 'A without Rh-factor'),
+        ('a+', 'A with Rh-factor'),
+        ('b-', 'B without Rh-factor'),
+        ('b+', 'B with Rh-factor'),
+    ], string="Blood Typing", required=1)
+
